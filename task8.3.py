dce2e85e02d67ed64666be92538dc78db498fa2f
Oleg Osipov <olossg@gmail.com>
2019-11-22 19:55:54 -0600

    'fix'

--- task8.3.py
+++ task8.3.py
@@ -5,11 +5,11 @@
 #
 
 class Employee:
-    def __init__(self, name, seniority, awards=None):
+    def __init__(self, name, seniority):
         self.name = name
         self.seniority = seniority
         self.grade = 1
-        self.awards = awards
+
 
     def grade_up(self):
         """Повышает уровень сотрудника"""
@@ -30,31 +30,42 @@ class Developer(Employee):
         self.seniority += 1
 
         # условие повышения сотрудника из презентации
-        if self.seniority % 5 == 0:
-            self.grade_up()
-
-        # публикация результатов
-        return self.publish_grade()
-
-class Designer(Employee):
-    def __init__(self, name, seniority, awards):
-        super().__init__(name, seniority, awards)
-        self.seniority = self.seniority + self.awards*2
-
-    def check_if_it_is_time_for_upgrade(self):
-
-        # для каждой аккредитации увеличиваем счетчик на 1
-        # пока считаем, что все разработчики проходят аккредитацию
-        self.seniority +=1
-
-        # условие повышения сотрудника из презентации
         if self.seniority % 7 == 0:
             self.grade_up()
 
         # публикация результатов
         return self.publish_grade()
 
-elena = Designer('Елена', seniority=0, awards=2)
+class Designer:
+    def __init__(self, name_d, seniority_d, awards):
+        self.name_d = name_d
+        self.seniority_d = seniority_d
+        self.grade_d = 1
+        self.awards = awards
+        self.seniority_d = self.seniority_d + self.awards * 2
+
+    def grade_up(self):
+        """Повышает уровень сотрудника"""
+        self.grade_d += 1
+
+    def publish_grade(self):
+        """Публикация результатов аккредитации сотрудников"""
+        print(self.name_d, self.grade_d)
+
+    def check_if_it_is_time_for_upgrade(self):
+
+        # для каждой аккредитации увеличиваем счетчик на 1
+        # пока считаем, что все разработчики проходят аккредитацию
+        self.seniority_d +=1
+
+        # условие повышения сотрудника из презентации
+        if self.seniority_d % 7 == 0:
+            self.grade_up()
+
+        # публикация результатов
+        return self.publish_grade()
+
+elena = Designer('Елена', seniority_d=0, awards=2)
 for i in range(20):
     elena.check_if_it_is_time_for_upgrade()
 print(elena)
